4dece93c8c84acf3158e4001b5c5da8b5202769e
emanuelbas <emanuelbastons@gmail.com>
2020-11-22 19:24:31 -0300

    Agregando funcionalidad para enrutamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
